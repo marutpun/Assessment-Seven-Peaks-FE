8d8d732c3bb5d8e59ac6c17ac067bfce94ea3476
Marut <marutpun@gmail.com>
2024-04-13 00:55:17 +0800

    changed to netlify function 2.0

--- netlify/functions/guardian.mjs
+++ netlify/functions/guardian.mjs
@@ -1,19 +1,34 @@
 const axios = require('axios');
-const apiUri = `https://content.guardianapis.com/search?api-key=`;
+const apiEndpoint = `https://content.guardianapis.com/search?api-key=`;
 const apiKey = Netlify.env.get('VITE_GUARDIAN_API_KEY');
 
 // Do not export default !!!
-export async function handler(event, context) {
+// Lambda Compatible (old)
+//
+// const apiKeyLambda = process.env.VITE_GUARDIAN_API_KEY;
+// export async function handler(event, context) {
+//   try {
+//     const { data } = await axios.get(`${apiEndpoint}${apiKey}`);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(data),
+//     };
+//   } catch (error) {
+//     return {
+//       statusCode: 401,
+//       body: JSON.stringify(error),
+//     };
+//   }
+// }
+
+// Netlify Function v2.0 (new)
+// https://answers.netlify.com/t/netlify-global-in-functions-v2/104887/14
+export default async (request, context) => {
   try {
-    const { data } = await axios.get(`${apiUri}${apiKey}`);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
+    const response = await axios.get(`${apiEndpoint}${apiKey}`);
+    return Response.json(response.data);
   } catch (error) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify(error),
-    };
+    console.log(error);
+    return Response.json({ error: 'Failed fetching data' }, { status: 500 });
   }
-}
+};
